07ade0a71eb92b3d1613b25e4047ce30c09d481d
Andrew <andrewdebyl@gmail.com>
2023-03-15 10:31:26 -0400

    adding challenges

---
+++ app.js
@@ -0,0 +1,126 @@
+//BEGINNER CHALLENGES
+
+// 1. Adding Two Numbers
+
+    // function addition (num1, num2) {
+    //     return num1 + num2;
+    // }
+
+    // console.log(addition(-1,-2));
+
+
+// 2. Convert Hours Into Seconds
+
+    // function hoursToSec(hours) {
+    //     return hours * 60 * 60;
+    // }
+
+    // console.log(hoursToSec(10))
+
+
+// 3. Perimeter of a Rectangle
+
+    // function perimeter (length, width) {
+    //     return (length*2) + (width*2);
+    // }
+
+    // console.log(perimeter(5,6));
+
+
+// 4. Area of a Triangle
+
+    // function areaTriangle (base, height) {
+    //     return base * height / 2;
+    // }
+
+    // console.log(areaTriangle(5, 10));
+
+
+// 5. Append to end of String
+
+    // function appendToEnd (string) {
+    //     return string + "Frontend";
+    // }
+
+    // console.log(appendToEnd("Helloooo"));
+
+
+// 6. Sum is Greater Than 100?
+
+    // function sumNumbers(num1, num2) {
+    //     return num1 + num2 > 100;
+    // }
+
+    // console.log(sumNumbers(51,50));
+
+
+// 7. Less Than or Equal to 0?
+
+    // function lessThanZero (num) {
+    //     return num <= 0;
+    // }
+
+    // console.log(lessThanZero(0));
+
+
+// 8. Return Opposite Boolean 
+
+    // function oppositeBoolean(bool) {
+    //     return !bool;
+    // }
+
+    // console.log(oppositeBoolean(true));
+
+
+// 9. Is Not the Number 0
+
+    // function notZero (num) {
+    //     return num !== 0;
+    // }
+
+    // console.log(notZero(null));
+
+
+// 10. Give the Remainder
+
+    // function calcRemainder (num1, num2) {
+    //     return num1 % num2;
+    // }
+
+    // console.log(calcRemainder(10,6));
+
+
+// 11. Is the Number Odd?
+
+    // function isNumOdd (num) {
+    //     return num % 2 !== 0
+    // }
+
+    // console.log(isNumOdd(11));
+
+
+// 12. Return Even or Odd
+
+    // function evenOrOdd (num) {
+    //     return num % 2 === 0 ? 1 : -1;
+    // }
+
+    // console.log(evenOrOdd(10));
+
+
+// 13. Check if Logged in and Subscribed 
+
+    // function checkLoginSub (string1, string2) {
+    //     return string1 === "LOGGED_IN" && string2 === "SUBSCRIBED" ? true : false;
+    // }
+
+    // console.log(checkLoginSub("LOGGED_IN", "SUBSCRIBED"));
+
+
+// 14. Check if Logged in or Subscribed 
+
+    // function checkLoginSub (string1, string2) {
+    //     return string1 === "LOGGED_IN" || string2 === "SUBSCRIBED" ? true : false;
+    // }
+
+    // console.log(checkLoginSub("LOGGED_IN", "SUBSCRIBED"));
